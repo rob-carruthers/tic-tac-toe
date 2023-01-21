21138dcf13fc64b43a3917039dc6af3bee39b7b1
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2023-01-21 17:34:47 +0000

    Create win logic

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
 const Player = (name) => {
-  return {name};
-}
+  return { name };
+};
 
 const gameBoard = (() => {
-  const board = new Array(9).fill("");
-  const players = [Player("X"), Player("O")]
+  let board = new Array(9).fill("");
+  const players = [Player("X"), Player("O")];
   let currentPlayerIndex = 0;
-  return { board, players, currentPlayerIndex };
+  let playing = true;
+
+  const isPlaying = () => playing;
+
+  const checkForWin = () => {
+    const winCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    winCombos.forEach((combo) => {
+      players.forEach((player) => {
+        if (
+          board[combo[0]] + board[combo[1]] + board[combo[2]] ===
+          player.name.repeat(3)
+        ) {
+          const resultDiv = document.getElementById("resultDiv");
+          resultDiv.textContent = player.name + " wins!";
+          playing = false;
+        }
+      });
+    });
+  };
+
+  return {
+    board,
+    playing,
+    isPlaying,
+    players,
+    currentPlayerIndex,
+    checkForWin,
+  };
 })();
 
 const displayController = (() => {
@@ -22,14 +60,14 @@ const displayController = (() => {
       gridItem.id = counter;
 
       gridItem.addEventListener("click", (event) => {
-        if (event.target.textContent === "") {
-          event.target.textContent = 
+        if (event.target.textContent === "" && gameBoard.isPlaying()) {
+          event.target.textContent =
             gameBoard.players[gameBoard.currentPlayerIndex].name;
-            gameBoard.currentPlayerIndex = 1 - gameBoard.currentPlayerIndex;
+          gameBoard.board[event.target.id] = event.target.textContent;
+          gameBoard.checkForWin();
+          gameBoard.currentPlayerIndex = 1 - gameBoard.currentPlayerIndex;
         }
-
       });
-
 
       boardDiv.appendChild(gridItem);
       counter++;
